903c1cfae5d5b21bccb122058514f1ebb6d8814f
Eric Zorn <zornwebdev@gmail.com>
2025-12-25 20:52:39 -0500

    fix: imports

--- apps/auth-api/src/router.ts
+++ apps/auth-api/src/router.ts
@@ -1,13 +1,11 @@
+import { INestApplication } from '@nestjs/common';
 import { ConnectRouter } from '@connectrpc/connect';
 
 import { elizav1 } from '@challah-social/protos-gen';
 
 import { AppService } from './app/app.service';
-import { INestApplication } from '@nestjs/common';
 
-export default (
-  app: INestApplication<any>
-): ((router: ConnectRouter) => void) => {
+export default (app: INestApplication): ((router: ConnectRouter) => void) => {
   const appService = app.get<AppService>(AppService);
 
   return (router: ConnectRouter) => {
