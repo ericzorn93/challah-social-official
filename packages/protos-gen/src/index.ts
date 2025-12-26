ed1a7275e93f76ba9b6b7a8b73d63f800fd9f55d
Eric Zorn <zornwebdev@gmail.com>
2025-12-25 21:02:32 -0500

    feat: clear placeholder service

--- packages/protos-gen/src/index.ts
+++ packages/protos-gen/src/index.ts
@@ -1,8 +1,5 @@
 // Buf Validate v1
 export * from './buf/validate/validate_pb.ts';
 
-// ConnectRPC Eliza v1
-export * as elizav1 from './connectrpc/eliza/v1/eliza_pb.ts';
-
 // Auth Service v1
 export * as authv1 from './auth/v1/auth_pb.ts';
