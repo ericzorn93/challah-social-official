075970bd9ffe0feb133e0476db04e30e4c9ac620
Eric Zorn <zornwebdev@gmail.com>
2025-12-25 21:11:13 -0500

    feat: listening on private network too

--- apps/auth-api/src/main.ts
+++ apps/auth-api/src/main.ts
@@ -7,6 +7,7 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { expressConnectMiddleware } from '@connectrpc/connect-express';
 import { createValidateInterceptor } from '@connectrpc/validate';
+import { createServer } from 'node:http';
 
 import routes from './router';
 import { AppModule } from './app/app.module';
@@ -22,9 +23,18 @@ async function bootstrap() {
     })
   );
 
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}`);
+  await app.init();
+  const httpAdapter = app.getHttpAdapter();
+
+  const server1 = createServer(httpAdapter.getInstance());
+  server1.listen(3000, '0.0.0.0');
+  Logger.log(`🚀 Application is running on: http://localhost:3000`);
+
+  if (process.env.NODE_ENV === 'production') {
+    const server2 = createServer(httpAdapter.getInstance());
+    server2.listen(3000, 'fly-local-6pn');
+    Logger.log(`🚀 Application is running on: http://fly-local-6pn:3000`);
+  }
 }
 
 bootstrap();
