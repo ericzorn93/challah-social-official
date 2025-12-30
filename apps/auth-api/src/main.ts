55534906870e5666582609c768391a306d2d8995
Eric Zorn <zornwebdev@gmail.com>
2025-12-30 10:45:49 -0500

    feat: servers start

--- apps/auth-api/src/main.ts
+++ apps/auth-api/src/main.ts
@@ -8,7 +8,6 @@ import { NestFactory } from '@nestjs/core';
 import { expressConnectMiddleware } from '@connectrpc/connect-express';
 import { createValidateInterceptor } from '@connectrpc/validate';
 import { createServer } from 'node:http';
-import { createServer as createServer2 } from 'node:http2';
 
 import routes from './router';
 import { AppModule } from './app/app.module';
@@ -27,13 +26,14 @@ async function bootstrap() {
   await app.init();
   const httpAdapter = app.getHttpAdapter();
 
-  const server1 = createServer(httpAdapter.getInstance());
-  server1.listen(3000, '0.0.0.0');
-  Logger.log(`🚀 Application is running on: http://localhost:3000`);
-
-  if (process.env.NODE_ENV === 'production') {
-    const server2 = createServer(httpAdapter.getInstance());
-    server2.listen(3000, 'fly-local-6pn');
+  if (process.env.NODE_ENV !== 'production') {
+    Logger.log('Running in development mode');
+    const server = createServer(httpAdapter.getInstance());
+    server.listen(3000, '0.0.0.0');
+    Logger.log(`🚀 Application is running on: http://localhost:3000`);
+  } else {
+    const server = createServer(httpAdapter.getInstance());
+    server.listen(3000, 'fly-local-6pn');
     Logger.log(`🚀 Application is running on: http://fly-local-6pn:3000`);
   }
 }
