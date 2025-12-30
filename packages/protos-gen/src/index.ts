c5e11c90c08b6778b282503a3144d368b6b7e29a
Eric Zorn <zornwebdev@gmail.com>
2025-12-30 13:03:10 -0500

    feat: adding random protos

--- packages/protos-gen/src/index.ts
+++ packages/protos-gen/src/index.ts
@@ -3,3 +3,6 @@ export * from './buf/validate/validate_pb.ts';
 
 // Auth Service v1
 export * as authv1 from './auth/v1/auth_pb.ts';
+
+// Random Service v1
+export * as randomv1 from './random/v1/random_pb.ts';
