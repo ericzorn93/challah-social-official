80beeb791c41428a5995d8a58a9a2b70728f560f
Eric Zorn <zornwebdev@gmail.com>
2026-01-03 13:46:56 -0500

    feat: talk between the auth and dating service

--- apps/auth-api/src/app/auth-v1/rpc.ts
+++ apps/auth-api/src/app/auth-v1/rpc.ts
@@ -1,13 +1,35 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { ServiceImpl } from '@connectrpc/connect';
+import { ConfigService } from '@nestjs/config';
+import { Client, createClient, ServiceImpl } from '@connectrpc/connect';
+import { createConnectTransport } from '@connectrpc/connect-node';
 
-import { authv1 } from '@challah-social/protos-gen';
+import { authv1, datingSettingsv1 } from '@challah-social/protos-gen';
 
 @Injectable()
 export class AuthV1RPCService
   implements ServiceImpl<typeof authv1.AuthService>
 {
   private readonly logger = new Logger(AuthV1RPCService.name);
+
+  private readonly datingSettingsClient: Client<
+    typeof datingSettingsv1.DatingSettingsService
+  >;
+
+  constructor(private readonly configService: ConfigService) {
+    const datingServiceURL =
+      this.configService.getOrThrow<string>('DATING_SERVICE_URL');
+
+    const transport = createConnectTransport({
+      httpVersion: '1.1',
+      baseUrl: datingServiceURL,
+    });
+    this.datingSettingsClient = createClient(
+      datingSettingsv1.DatingSettingsService,
+      transport
+    );
+
+    this.logger.log('DatingSettingsClient initialized');
+  }
 
   // @ts-expect-error: Login Request
   public async login(_req: authv1.LoginRequest): Promise<authv1.LoginResponse> {
@@ -24,6 +46,9 @@ export class AuthV1RPCService
     _req: authv1.LogoutRequest
   ): Promise<authv1.LogoutResponse> {
     this.logger.log('Logout request received');
+
+    //
+    await this.datingSettingsClient.placeholder({});
 
     return {
       $typeName: 'auth.v1.LogoutResponse',
