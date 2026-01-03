ffbe13aa482abc8c5060af069504120f6ffe1d1c
Eric Zorn <zornwebdev@gmail.com>
2026-01-03 12:59:18 -0500

    feat: adding auth protos

--- packages/protos-gen/src/index.ts
+++ packages/protos-gen/src/index.ts
@@ -3,3 +3,6 @@ export * from './buf/validate/validate_pb.ts';
 
 // Dating Settings Service v1
 export * as datingSettingsv1 from './dating-settings/v1/dating-settings_pb.ts';
+
+// Auth Service v1
+export * as authv1 from './auth/v1/auth_pb.ts';
